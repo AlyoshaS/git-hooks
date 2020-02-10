455d4bc94a72a98862e8aae0ea660c3676d3f226
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-02-10 18:46:37 -0300

    feat: added getRepos function to display my repositories with github api.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  return <h1>Hi, hooks!</h1>
+  const [repositories, setRepositories] = useState([]);
+
+  // componentDidMount
+  useEffect(() => {
+    async function getRepos() {
+      const response = await fetch('https://api.github.com/users/AlyoshaS/repos')
+      const data = await response.json();
+
+      setRepositories(data)
+    }
+    getRepos()
+  }, []);
+
+  return (
+    <>
+      <ul>
+        { repositories.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        )) }
+      </ul>
+    </>
+  )
 }
