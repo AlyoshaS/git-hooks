b8be3e0f25b4b01e2689033ebea508502e2787d9
Evelyn Schmitz <evelynschmitz1@gmail.com>
2020-02-10 19:23:37 -0300

    feat: added favorite repositories

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  // componentDidMount
   useEffect(() => {
     async function getRepos() {
       const response = await fetch('https://api.github.com/users/AlyoshaS/repos')
@@ -14,11 +13,29 @@ export default function App() {
     getRepos()
   }, []);
 
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <>
       <ul>
         { repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span> (Favorito) </span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         )) }
       </ul>
     </>
